fd6e7f1a484d55dc29d49a5153a0a406bd853ebf
sanjeeth r <9209035+sanjeeth-07@users.noreply.github.com>
2019-10-02 16:27:52 +0100

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld 
+{ 
+    // my helloworld program in java
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+    } 
+} 
