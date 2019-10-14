4b97411b77f3b5b660719e7639ed04598d574740
sanjeeth r <9209035+sanjeeth-07@users.noreply.github.com>
2019-10-14 17:14:33 +0100

    Create timezones.java

---
+++ timezones.java
@@ -0,0 +1,16 @@
+public class TimezoneDisplayApp {
+ 
+    public static void main(String... args) {
+        TimezoneDisplay display = new TimezoneDisplay();
+ 
+        System.out.println("Time zones in UTC:");
+        List<String> utc = display.getTimeZoneList(
+          TimezoneDisplay.OffsetBase.UTC);
+        utc.forEach(System.out::println);
+ 
+        System.out.println("Time zones in GMT:");
+        List<String> gmt = display.getTimeZoneList(
+          TimezoneDisplay.OffsetBase.GMT);
+        gmt.forEach(System.out::println);
+    }
+}
